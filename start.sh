94a1e05333450f2d59bb384cea842ffc23a463f9
Tom <twig87@gmail.com>
2019-08-17 19:59:50 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,4 +7,4 @@ mv -v data-temp/*/!(server.properties|ops.json|banned-ips.json|banned-players.js
 rm -r -f /data-temp 
 rm -r -f ./mc.zip
 jarfile=$(cd /data && ls|grep forge-*.jar)
-cd /data && java -server "-Xmx${INIT_MEM}" "-Xms${MAX_MEM}" -jar $jarfile nogui
+cd /data && java -server "-Xmx${INIT_MEM}" "-Xms${MAX_MEM}" "-Dfml.queryResult=confirm" -jar $jarfile nogui
